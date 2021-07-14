7fa228014321d4ac0a36371e01f405b49734f614
Juan Villarreal <70031233+juancv3d@users.noreply.github.com>
2021-07-14 18:47:35 -0500

    Update .create_command.sh

--- .create_command.sh
+++ .create_command.sh
@@ -3,11 +3,11 @@
 function create(){ 
   cd 
   python3 git_automation.py -r $1
-  cd /mnt/c/Users/juanc/Documents/Python_Projects
+  cd 'insert here your project directory'
   mkdir $1
   cd $1
   git init
-  git remote add origin https://github.com/juancv3d/$1.git
+  git remote add origin https://github.com/<username>/$1.git
   touch README.md
   git add .
   git commit -m "Initial commit"
